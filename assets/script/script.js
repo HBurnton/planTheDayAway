6c6db14e5e963b0ae214e40076e58284109abe9d
hburnton <hburnton@gmail.com>
2022-08-27 11:27:46 -0700

    allow storage and retrieval

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,34 +2,54 @@
 var today = moment().format("dddd, MMMM Do");
 var thisHour = moment().format("hA");
 var hourOffset = 9;
+var tasks = Array(9).fill('');
 
-
-// Big thanks to this Stack Overflow for showing how to do it all in one string
-//https://stackoverflow.com/questions/34678098/append-html-block-jquery
-for(var i=0; i<=8; i++){
-    var hourOfDay = moment(i+hourOffset, 'h').format("hA");
-    if(moment(thisHour, 'hA').isAfter(moment(hourOfDay,'hA'))){
-        hourColor = "past"
-    }else{
-    //console.log(moment(thisHour, 'hA').isAfter(moment(hourOfDay,'hA')));
-    if(moment(thisHour, 'hA').isSame(moment(hourOfDay,'hA'))){
-        hourColor = "present"
-    }else{
-        hourColor = "future";
-    }}
-
-    $('.container').append( "<div class=\"row\" id="+i+"> <div class=\"hour d-flex justify-content-center align-items-center col-md-1\">"+ hourOfDay +"</div><textarea class=\"col-md-10 "+hourColor+"\"></textarea><div class=\"saveBtn col-md-1 align-middle d-flex justify-content-center align-items-center\"><i class=\"fa-solid fa-floppy-disk\"></i></div></div>")
-    
-
-}
-
-setInterval(function () {thisHour = moment().format("hA")}, 5000);
-
+//set text at top to current day
 $('#currentDay').text(today);
 
-function saveMe(event){
-    var button1 = $(event.target);
-    console.log(button1.parent().parent().children('textarea').val());
+//function checks for prior stored tasks and if so, sets tasks equal to them
+function loadPriorTasks() {
+    var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTasks !== null) {
+        tasks = storedTasks;
+    }
 }
 
+for (var i = 0; i <= 8; i++) {
+
+    loadPriorTasks();//loads tasks
+
+    var hourOfDay = moment(i + hourOffset, 'h').format("hA");
+
+    //compares this current hour to the hour of day that is being crated in row and sets color depending on relationship. 
+    if (moment(thisHour, 'hA').isAfter(moment(hourOfDay, 'hA'))) {
+        hourColor = "past";
+    } else if (moment(thisHour, 'hA').isSame(moment(hourOfDay, 'hA'))) {
+        hourColor = "present";
+    } else {
+        hourColor = "future";
+    }
+
+    /*
+    This bad boy puts everything together. Makes row with unique ids, sets the hour color and places content of textarea boxes.
+
+    Big thanks to this Stack Overflow for showing how to do it all in one string
+    https://stackoverflow.com/questions/34678098/append-html-block-jquery
+    */
+    $('.container').append("<div class=\"row\" id=" + i + "> <div class=\"hour d-flex justify-content-center align-items-center col-md-1\">" + hourOfDay + "</div><textarea class=\"col-md-10 " + hourColor + "\">" + tasks[i] + "</textarea><div class=\"saveBtn col-md-1 align-middle d-flex justify-content-center align-items-center\"><i class=\"fa-solid fa-floppy-disk\"></i></div></div>")
+
+
+}
+
+    //Gets event target, finds the parent row and uses id as index to store row's child text area content. Stringify and save array as 'tasks'
+function saveMe(event) {
+    var button1 = $(event.target);
+    var row = button1.parent().parent();
+    tasks[Number(row.attr('id'))] = row.children('textarea').val(); 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//event listener on the save icon triggers SaveMe function
 $('i').click(saveMe);
+
+//setInterval(function () { thisHour = moment().format("hA") }, 5000); Maybe going to live update color at some point?
