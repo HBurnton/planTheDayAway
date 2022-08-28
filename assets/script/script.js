2e8a39470cb3477d5890cfbb9b78c2695e60d6a6
hburnton <hburnton@gmail.com>
2022-08-28 10:23:54 -0700

    restructure javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,9 +4,6 @@ var thisHour = moment().format("hA");
 var hourOffset = 9;
 var tasks = Array(9).fill('');
 
-//set text at top to current day
-$('#currentDay').text(today);
-
 //function checks for prior stored tasks and if so, sets tasks equal to them
 function loadPriorTasks() {
     var storedTasks = JSON.parse(localStorage.getItem("tasks"));
@@ -15,9 +12,20 @@ function loadPriorTasks() {
     }
 }
 
-for (var i = 0; i <= 8; i++) {
+//Gets event target, finds the parent row and uses id as index to store row's child text area content. Stringify and save array as 'tasks'
+function saveMe(event) {
+    var button1 = $(event.target);
+    var row = button1.parent().parent();
+    tasks[Number(row.attr('id'))] = row.children('textarea').val(); 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
-    loadPriorTasks();//loads tasks
+//set text at top to current day
+$('#currentDay').text(today);
+
+loadPriorTasks();//loads tasks
+
+for (var i = 0; i <= 8; i++) {
 
     var hourOfDay = moment(i + hourOffset, 'h').format("hA");
 
@@ -30,23 +38,15 @@ for (var i = 0; i <= 8; i++) {
         hourColor = "future";
     }
 
-    /*
-    This bad boy puts everything together. Makes row with unique ids, sets the hour color and places content of textarea boxes.
-
-    Big thanks to this Stack Overflow for showing how to do it all in one string
-    https://stackoverflow.com/questions/34678098/append-html-block-jquery
-    */
-    $('.container').append("<div class=\"row\" id=" + i + "> <div class=\"hour d-flex justify-content-center align-items-center col-md-1\">" + hourOfDay + "</div><textarea class=\"col-md-10 " + hourColor + "\">" + tasks[i] + "</textarea><div class=\"saveBtn col-md-1 align-middle d-flex justify-content-center align-items-center\"><i class=\"fa-solid fa-floppy-disk\"></i></div></div>")
-
-
-}
-
-    //Gets event target, finds the parent row and uses id as index to store row's child text area content. Stringify and save array as 'tasks'
-function saveMe(event) {
-    var button1 = $(event.target);
-    var row = button1.parent().parent();
-    tasks[Number(row.attr('id'))] = row.children('textarea').val(); 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    $('.container').append(`<div class="row" id= "${i}"> 
+                                <div class="hour d-flex justify-content-center align-items-center col-md-1">
+                                    ${hourOfDay}
+                                </div>
+                                <textarea class="col-md-10 ${hourColor}">${tasks[i]}</textarea>
+                                <div class="saveBtn col-md-1 align-middle d-flex justify-content-center align-items-center">
+                                    <i class="fa-solid fa-floppy-disk"></i>
+                                </div>
+                            </div>`);
 }
 
 //event listener on the save icon triggers SaveMe function
